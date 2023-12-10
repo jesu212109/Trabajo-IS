d688bfddab8168a4b249f74146bb55ea2edc10bb
Javier Espantaleón <i22espef@uco.es>
2023-12-10 20:17:10 +0100

    Formatear fichero dummy_data

--- data/dummy_data.sql
+++ data/dummy_data.sql
@@ -1,9 +1,51 @@
-INSERT INTO Usuarios (Nombre, CorreoElectronico, Contraseña, Rol) VALUES
-    ('Roberto', 'roberto@example.com', 'passRoberto', 'Organizador'),
-    ('Manuel', 'manuel@example.com', 'passMaria', 'DirectorAcademico'),
-    ('Juan', 'juan@example.com', 'passJuan', 'Visitante'),
-    ('Laura', 'laura@example.com', 'passLaura', 'Visitante'),
-    ('Carlos', 'carlos@example.com', 'passCarlos', 'Registrado'),
-    ('Ana', 'ana@example.com', 'passAna', 'Registrado'),
-    ('Eduardo', 'eduardo@example.com', 'passEduardo', 'Ponente'),
-    ('Sofía', 'sofia@example.com', 'passSofia', 'Ponente');
+INSERT INTO
+    Usuarios (Nombre, CorreoElectronico, Contraseña, Rol)
+VALUES
+    (
+        'Roberto',
+        'roberto@example.com',
+        'passRoberto',
+        'Organizador'
+    ),
+    (
+        'Manuel',
+        'manuel@example.com',
+        'passMaria',
+        'DirectorAcademico'
+    ),
+    (
+        'Juan',
+        'juan@example.com',
+        'passJuan',
+        'Visitante'
+    ),
+    (
+        'Laura',
+        'laura@example.com',
+        'passLaura',
+        'Visitante'
+    ),
+    (
+        'Carlos',
+        'carlos@example.com',
+        'passCarlos',
+        'Registrado'
+    ),
+    (
+        'Ana',
+        'ana@example.com',
+        'passAna',
+        'Registrado'
+    ),
+    (
+        'Eduardo',
+        'eduardo@example.com',
+        'passEduardo',
+        'Ponente'
+    ),
+    (
+        'Sofía',
+        'sofia@example.com',
+        'passSofia',
+        'Ponente'
+    );
