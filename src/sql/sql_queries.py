b94db9e3e43ee7ff3ac12cbacb08cad3d0be5ff0
Javier Espantaleón <i22espef@uco.es>
2023-12-11 19:17:53 +0100

    Correción indentación función buscar_usuario_por_credenciales

--- src/sql/sql_queries.py
+++ src/sql/sql_queries.py
@@ -1,8 +1,7 @@
 import mysql.connector
 
 def buscar_usuario_por_credenciales(cursor, correo_electronico, contraseña): # Busca si el ususario está en la base de datos
-
-            query = "SELECT * FROM Usuarios WHERE CorreoElectronico = %s AND Contraseña = %s"
-            cursor.execute(query, (correo_electronico, contraseña))
-            resultado = cursor.fetchone()
-            return resultado
+    query = "SELECT * FROM Usuarios WHERE CorreoElectronico = %s AND Contraseña = %s"
+    cursor.execute(query, (correo_electronico, contraseña))
+    resultado = cursor.fetchone()
+    return resultado
