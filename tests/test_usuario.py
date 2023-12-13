49f0b833a524b2537649309c4df18f1c0a68fdc8
Javier Espantaleón <i22espef@uco.es>
2023-12-13 20:45:20 +0100

    Crear test_usuario.py

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,1 +1,14 @@
+import pytest
+from src.classes.usuario import Usuario
 
+@pytest.fixture
+def usuario_de_prueba():
+    # Aquí puedes inicializar un objeto de Usuario para usarlo en las pruebas
+    return Usuario(id_usuario=1, nombre="Usuario1", correo_electronico="usuario1@example.com", contraseña="secreto", rol="Registrado")
+
+def test_actualizar_informacion(usuario_de_prueba):
+    # Act: Llama al método que quieres probar
+    usuario_de_prueba.actualizar_informacion("NuevoNombre")
+
+    # Assert: Verifica que la instancia del Usuario se actualizó correctamente
+    assert usuario_de_prueba.nombre == "NuevoNombre"
