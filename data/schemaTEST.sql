89372fc5c4ed38e095d0fb7f7e73d8e206bd6009
javieresp-uco <150701436+javieresp-uco@users.noreply.github.com>
2023-12-17 19:17:42 +0100

    Actualizar esquema base de datos de testeo

--- data/schemaTEST.sql
+++ data/schemaTEST.sql
@@ -1,68 +1,50 @@
 use testing;
 
--- Tabla de Usuarios
-CREATE TABLE Usuarios (
-    IDUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    Nombre VARCHAR(50) NOT NULL,
-    CorreoElectronico VARCHAR(100) NOT NULL,
-    Contraseña VARCHAR(255) NOT NULL,
-    Rol ENUM(
-        'Organizador',
-        'DirectorAcademico',
-        'Visitante',
-        'Registrado',
-        'Ponente'
-    ) NOT NULL
-);
+CREATE TABLE `usuarios` (
+  `IDUsuario` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `CorreoElectronico` varchar(100) NOT NULL,
+  `Contraseña` varchar(255) NOT NULL,
+  `Rol` enum('Organizador','DirectorAcademico','Visitante','Registrado','Ponente') NOT NULL,
+  PRIMARY KEY (`IDUsuario`)
+)
 
--- Tabla de Eventos Académicos
-CREATE TABLE Eventos (
-    IDEvento INT PRIMARY KEY AUTO_INCREMENT,
-    Titulo VARCHAR(100) NOT NULL,
-    Descripcion TEXT,
-    FechaInicio DATE NOT NULL,
-    FechaFin DATE NOT NULL,
-    Aforo INT NOT NULL,
-    Ubicacion VARCHAR(100) NOT NULL,
-    Precio DECIMAL(10, 2) NOT NULL,
-    DerechosParticipacion TEXT,
-    FOREIGN KEY (IDUsuarioOrganizador) REFERENCES Usuarios(IDUsuario)
-);
+CREATE TABLE `preinscripciones` (
+  `IDPreinscripcion` int NOT NULL AUTO_INCREMENT,
+  `IDEvento` int DEFAULT NULL,
+  `IDUsuarioRegistrado` int DEFAULT NULL,
+  `FechaPreinscripcion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `Confirmado` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`IDPreinscripcion`),
+  KEY `IDUsuarioRegistrado` (`IDUsuarioRegistrado`),
+  KEY `IDEvento` (`IDEvento`),
+  CONSTRAINT `preinscripciones_ibfk_1` FOREIGN KEY (`IDUsuarioRegistrado`) REFERENCES `usuarios` (`IDUsuario`),
+  CONSTRAINT `preinscripciones_ibfk_2` FOREIGN KEY (`IDEvento`) REFERENCES `eventos` (`IDEvento`)
+)
 
--- Tabla de Preinscripciones
-CREATE TABLE Preinscripciones (
-    IDPreinscripcion INT PRIMARY KEY AUTO_INCREMENT,
-    IDEvento INT,
-    IDUsuarioRegistrado INT,
-    FechaPreinscripcion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Confirmado BOOLEAN DEFAULT 0,
-    FOREIGN KEY (IDUsuarioRegistrado) REFERENCES Usuarios(IDUsuario),
-    FOREIGN KEY (IDEvento) REFERENCES Eventos(IDEvento)
-);
+CREATE TABLE `eventos` (
+  `IDEvento` int NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(100) NOT NULL,
+  `Descripcion` text,
+  `FechaInicio` date DEFAULT NULL,
+  `FechaFin` date DEFAULT NULL,
+  `Aforo` int DEFAULT NULL,
+  `Ubicacion` varchar(100) DEFAULT NULL,
+  `Precio` decimal(10,2) DEFAULT NULL,
+  `DerechosParticipacion` text,
+  `IDUsuarioOrganizador` int DEFAULT NULL,
+  PRIMARY KEY (`IDEvento`),
+  KEY `IDUsuarioOrganizador` (`IDUsuarioOrganizador`),
+  CONSTRAINT `eventos_ibfk_1` FOREIGN KEY (`IDUsuarioOrganizador`) REFERENCES `usuarios` (`IDUsuario`)
+)
 
--- Tabla de Ponentes
-CREATE TABLE Ponentes (
-    IDPonente INT PRIMARY KEY AUTO_INCREMENT,
-    IDEvento INT,
-    IDUsuario INT,
-    FOREIGN KEY (IDUsuario) REFERENCES Usuarios(IDUsuario),
-    FOREIGN KEY (IDEvento) REFERENCES Eventos(IDEvento)
-);
-
--- Tabla de Certificados
-CREATE TABLE Certificados (
-    IDCertificado INT PRIMARY KEY AUTO_INCREMENT,
-    IDPreinscripcion INT,
-    Contenido TEXT,
-    FOREIGN KEY (IDPreinscripcion) REFERENCES Preinscripciones(IDPreinscripcion)
-);
-
--- Tabla de Correos Electrónicos
-CREATE TABLE Correos (
-    IDCorreo INT PRIMARY KEY AUTO_INCREMENT,
-    IDUsuario INT,
-    Asunto VARCHAR(255) NOT NULL,
-    Cuerpo TEXT NOT NULL,
-    Enviado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (IDUsuario) REFERENCES Usuarios(IDUsuario)
-);
+CREATE TABLE `correos` (
+  `IDCorreo` int NOT NULL AUTO_INCREMENT,
+  `IDUsuario` int DEFAULT NULL,
+  `Asunto` varchar(255) NOT NULL,
+  `Cuerpo` text NOT NULL,
+  `Enviado` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`IDCorreo`),
+  KEY `IDUsuario` (`IDUsuario`),
+  CONSTRAINT `correos_ibfk_1` FOREIGN KEY (`IDUsuario`) REFERENCES `usuarios` (`IDUsuario`)
+)
