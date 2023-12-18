bf48240d9ca100fb9d642612721d23dc4b93ddd8
javieresp-uco <150701436+javieresp-uco@users.noreply.github.com>
2023-12-18 23:55:30 +0100

    Actualizar funcionalidad mailing utilizando smtplib

--- src/app/screens/mailing.py
+++ src/app/screens/mailing.py
@@ -1,14 +1,32 @@
-class EmailSegmentation:
-    def __init__(self):
-        self.emails = {
-            'usuario1@example.com': {'nombre': 'Usuario1', 'edad': 25, 'ubicacion': 'Ciudad A'},
-            'usuario2@example.com': {'nombre': 'Usuario2', 'edad': 35, 'ubicacion': 'Ciudad B'},
-        }
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-    def segmentar_por_edad(self, edad_limite):
-        segmento = {email: info for email, info in self.emails.items() if info['edad'] < edad_limite}
-        return segmento
+class Mailing:
+    def __init__(self, smtp_server, smtp_port, sender_email, sender_password):
+        self.smtp_server = smtp_server
+        self.smtp_port = smtp_port
+        self.sender_email = sender_email
+        self.sender_password = sender_password
 
-    def segmentar_por_ubicacion(self, ciudad):
-        segmento = {email: info for email, info in self.emails.items() if info['ubicacion'] == ciudad}
-        return segmento
+    def send_mail(self, to_email, subject, body):
+        # Crear el objeto del mensaje
+        message = MIMEMultipart()
+        message["From"] = self.sender_email
+        message["To"] = to_email
+        message["Subject"] = subject
+
+        # Añadir el cuerpo del mensaje
+        message.attach(MIMEText(body, "plain"))
+
+        # Configurar la conexión segura
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port, context=context) as server:
+            # Iniciar sesión en el servidor SMTP
+            server.login(self.sender_email, self.sender_password)
+
+            # Enviar el mensaje
+            server.sendmail(self.sender_email, to_email, message.as_string())
+        
